063c57dd660b9886a76e9be40941193cbe5469f2
Kashmiri <153292920+niwittko@users.noreply.github.com>
2024-10-27 18:32:15 +0100

    Aufgabe 2_1

---
+++ OOP_U_2/src/AnwendersystemControl.java
@@ -0,0 +1,7 @@
+
+public class AnwendersystemControl {
+	
+	private AnwendersystemModel anwModel;
+	private AnwendersystemView anwView;
+
+}
---
+++ OOP_U_2/src/AnwendersystemView.java
@@ -0,0 +1,7 @@
+
+public class AnwendersystemView {
+	
+	private AnwendersystemControl anwControl;
+	private AnwendersystemModel anwModel;
+
+}
